c5df5b82e9c7f44dc72f5351dbbf2b2517a26690
litlighilit <litlighilit@foxmail.com>
2025-04-10 15:25:01 +0800

    doc(main): add module doc

--- src/t_ci.nim
+++ src/t_ci.nim
@@ -1,3 +1,4 @@
+## test CI
 # This is just an example to get you started. A typical library package
 # exports the main API in this file. Note that you cannot rename this file
 # but you can remove it if you wish.
